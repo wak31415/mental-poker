01a89092ae4a64a6b2c2e61f8ac58b4b47ff77c7
Mihails Valtusovs <mihails.valtusovs@polytechnique.edu>
2021-04-30 11:19:21 +0200

    remove target directory from the repo

--- target/debug/deps/mental_poker-501592561f04dac5.d
+++
@@ -1,5 +0,0 @@
-/home/olavi/mental-poker/target/debug/deps/mental_poker-501592561f04dac5: src/main.rs
-
-/home/olavi/mental-poker/target/debug/deps/mental_poker-501592561f04dac5.d: src/main.rs
-
-src/main.rs:
