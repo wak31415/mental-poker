ce867401d36dff2d8e43aff73a05d128e6f676d1
Mihails Valtusovs <mihails.valtusovs@polytechnique.edu>
2021-05-14 13:09:01 +0200

    fix coin_flipping, it works!

--- src/coin_flipping.rs
+++ src/coin_flipping.rs
@@ -9,32 +9,31 @@
 
 // mod prime_gen;
 use super::prime_gen;
-mod quadratic_residues;
-mod astarzstar;
-mod jacobi;
-mod gcd;
+use super::quadratic_residues;
+use super::astarzstar;
+use super::jacobi;
+use super::gcd;
 use rand::prelude::*;
+use num_bigint::BigUint;
+use num_traits::One;
 
 fn check_quadratic_residuoity(y: &BigUint, N: &BigUint, P: &BigUint, Q: &BigUint) -> bool {
-    if gcd::compute(y, N) != 1 {
+    if !(gcd::compute(y, N).is_one()) {
         return false;
     }   
     if jacobi::compute(y, N) == -1 {
         return false;
     }
-    let qr_p : bool = quadratic_residues::is_quadratic_residue(y, P);
-    let qr_q : bool = quadratic_residues::is_quadratic_residue(y, Q);
-
-    return (qr_p && qr_q);
+    return quadratic_residues::is_n(y, P, Q);
 }
 
-// Returns A_N, N, y, P, Q
+// Returns N, y, P, Q
 pub fn generate_keys(size: usize) -> (BigUint, BigUint, BigUint , BigUint) {
     // I really hate constants - Mihails
     let P = prime_gen::gen_prime(size);
     let Q = prime_gen::gen_prime(size);
 
-    let N = P * Q;
+    let N = &P * &Q;
 
     let mut y = astarzstar::rand_astar(&N);
     while !check_quadratic_residuoity(&y, &N, &P, &Q) {
@@ -48,8 +47,10 @@ pub fn generate_keys(size: usize) -> (BigUint, BigUint, BigUint , BigUint) {
 
 pub fn guess_whether_quadratic_residue(N: &BigUint, y: &BigUint, q: &BigUint) -> bool {
     let jacobi = jacobi::compute(q, N);
-
-    if (jacobi == -1) {
+    if jacobi == -1 {
+        return false;
+    }
+    if y == q {
         return false;
     }
 
