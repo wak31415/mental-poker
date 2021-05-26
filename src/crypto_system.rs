1a70aebd67564624bc50ba88233cfdf0f0cd50bc
William Koch <william.koch@polytechnique.edu>
2021-05-27 00:06:35 +0200

    Create key structs and assign values to cards

--- src/crypto_system.rs
+++ src/crypto_system.rs
@@ -1,31 +1,60 @@
 use super::astarzstar;
 use super::quadratic_residues::is_n;
+use super::card_deck;
 use num_bigint::{BigUint};
 
-type Message = Vec<bool>;
-type Ciphertext = Vec<BigUint>;
+pub type Plaintext = Vec<bool>;
+pub type Ciphertext = Vec<BigUint>;
 
-pub fn encrypt(message: &Message, n: &BigUint, y: &BigUint) -> Ciphertext {
-    let mut res: Ciphertext = Vec::<BigUint>::with_capacity(message.len());
-    for i in 0..message.len() {
-        let xi = astarzstar::rand_zstar(n);
-        if message[i] {
-            res.push((y*&xi*&xi) % n);
-        } else {
-            res.push(xi.modpow(&BigUint::from(2u32), n));
-        }
-    }
-    return res;
+pub struct PrivateKey {
+    pub p: BigUint,
+    pub q: BigUint
 }
 
-pub fn decrypt(cipher: &Ciphertext, p1: &BigUint, p2: &BigUint) -> Message {
-    let mut res = Message::with_capacity(cipher.len());
+pub struct PublicKey {
+    pub N: BigUint,
+    pub y: BigUint
+}
+
+pub fn num_to_plaintext(num: u8) -> Plaintext {
+    let mut pt : Plaintext = Vec::new();
+    let mut tmp = num;
+    for _i in 0..6 {
+        pt.push((tmp&1) != 0);
+        tmp = tmp >> 1;
+    }
+    pt
+
+}
+
+pub fn encrypt(message: &Plaintext, pubkey: &PublicKey) -> Ciphertext {
+    let mut res: Ciphertext = Vec::<BigUint>::with_capacity(message.len());
+    for i in 0..message.len() {
+        let xi = astarzstar::rand_zstar(&pubkey.N);
+        if message[i] {
+            res.push((&pubkey.y*&xi*&xi) % &pubkey.N);
+        } else {
+            res.push(xi.modpow(&BigUint::from(2u32), &pubkey.N));
+        }
+    }
+    res
+}
+
+
+pub fn encrypt_card(message: card_deck::Card, pubkey: &PublicKey) -> card_deck::EncrCard {
+    let encr_card = card_deck::EncrCard { N: pubkey.N.clone(), ciphertext: encrypt(&message.plaintext, &pubkey) };
+    encr_card
+}
+
+
+pub fn decrypt(cipher: &Ciphertext, privkey: PrivateKey) -> Plaintext {
+    let mut res = Plaintext::with_capacity(cipher.len());
     for i in 0..cipher.len() {
-        if is_n(&cipher[i], p1, p2) {
+        if is_n(&cipher[i], &privkey.p, &privkey.q) {
             res.push(false);
         } else {
             res.push(true);
         }
     }
-    return res;
+    res
 }
--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,39 @@ mod crypto_system;
 mod gcd;
 mod jacobi;
 mod quadratic_residues;
+mod card_deck;
 
 fn main() {
+    let cards = card_deck::gen_card_deck(32, 8);
+    println!("{:?}", cards);
+
+    
     let p1 = prime_gen::gen_prime(512);
     let mut p2 = prime_gen::gen_prime(512);
     while &p1 == &p2 {
         p2 = prime_gen::gen_prime(512);
     }
+    
     println!("Generated primes");
     //p1 and p2 are unequal primes (private key)
-    let n = &p1*&p2; //n is the public key together with some non-residue y to be computed
+    let privkey = crypto_system::PrivateKey{p: p1, q: p2};
+    let n = &privkey.p*&privkey.q; //n is the public key together with some non-residue y to be computed
     let mut y = astarzstar::rand_astar(&n); //astar (vec<bigUInt>) is the coprimes of n with jacobi symbol 1
-    while quadratic_residues::is_quadratic_residue_n(&y, &p1, &p2) {
+    while quadratic_residues::is_n(&y, &privkey.p, &privkey.q) {
         //We regenerate y until its not a quadratic residue
         y = astarzstar::rand_astar(&n);
     }
     //Now (n, y) is the public key
     let message: Vec<bool> = vec![true, true, false, false, true];
-    let cipher = crypto_system::encrypt(&message, &n, &y);
+    let pubkey = crypto_system::PublicKey{N: n, y: y};
+    
+    let cipher = crypto_system::encrypt(&message, &pubkey);
     for i in 0..5 {
         println!("{}", cipher[i]);
     }
-    let decrypted_message = crypto_system::decrypt(&cipher, &p1, &p2);
+    let decrypted_message = crypto_system::decrypt(&cipher, privkey);
     for i in 0..5 {
         println!("{}", decrypted_message[i]);
     }
+    // println!("{:?}", crypto_system::encrypt_card(cards[0], &pubkey));
 }
