7863c5ff1eaf73015ffdf08cdd8f8f8b8a8b55d9
William Koch <william.koch@polytechnique.edu>
2021-05-27 11:48:05 +0200

    Propagate private and public key changes to coin flipping

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,68 @@ mod gcd;
 mod jacobi;
 mod quadratic_residues;
 mod card_deck;
+mod coin_flipping;
+
+use crypto_system::{Ciphertext, Plaintext};
 
 fn main() {
-    let cards = card_deck::gen_card_deck(32, 8);
-    println!("{:?}", cards);
+    let (privkey, pubkey) = coin_flipping::generate_keys(10);
+    let q = astarzstar::rand_astar(&pubkey.N);
 
-    
-    let p1 = prime_gen::gen_prime(32);
-    let mut p2 = prime_gen::gen_prime(32);
-    while &p1 == &p2 {
-        p2 = prime_gen::gen_prime(32);
-    }
-    
-    println!("Generated primes");
-    //p1 and p2 are unequal primes (private key)
-    let privkey = crypto_system::PrivateKey{p: p1, q: p2};
-    let n = &privkey.p*&privkey.q; //n is the public key together with some non-residue y to be computed
-    let mut y = astarzstar::rand_astar(&n); //astar (vec<bigUInt>) is the coprimes of n with jacobi symbol 1
-    while quadratic_residues::is_n(&y, &privkey.p, &privkey.q) {
-        //We regenerate y until its not a quadratic residue
-        y = astarzstar::rand_astar(&n);
-    }
-    //Now (n, y) is the public key
-    let message: Vec<bool> = vec![true, true, false, false, true];
-    let pubkey = crypto_system::PublicKey{N: n, y: y};
-    
-    let cipher = crypto_system::encrypt(&message, &pubkey);
-    for i in 0..5 {
-        println!("{}", cipher[i]);
-    }
-    let decrypted_message = crypto_system::decrypt(&cipher, &privkey);
-    for i in 0..5 {
-        println!("{}", decrypted_message[i]);
-    }
+    let b_answers = coin_flipping::guess_whether_quadratic_residue(&pubkey, &q);
 
-    println!("{:?}", cards[0]);
-    let encr_card : card_deck::EncrCard = crypto_system::encrypt_card(&cards[0], &pubkey);
-    println!("{:?}", encr_card);
-    println!("{:?}", crypto_system::decrypt_card(&Box::new(encr_card), &privkey));
+    let mut s_b : String = String::from("");
+    if b_answers { s_b = String::from("is") } else { s_b = String::from("is not") }
+
+    let a_knows = quadratic_residues::is_n(&q, &privkey.p, &privkey.q);
+
+    let mut s_a : String = String::from("");
+    if a_knows { s_a = String::from("is") } else { s_a = String::from("is not") }
+
+    let mut s_w : String = String::from("");
+    if b_answers == a_knows { s_w = String::from("won") } else { s_w = String::from("lost") }
+
+    println!("One run of coin-flipping-in-a-well: ");
+    println!("A generated N = {}, y = {}, P = {}, Q = {}; ", pubkey.N, pubkey.y, privkey.p, privkey.q);
+    println!("A generated q = {}; ", q);
+    println!("B guessed that q {} a q.r. mod N; ", s_b);
+    println!("A knows that q {} a q.r. mod N; ", s_a);
+    println!("B {} this coin flip.", s_w);
+
+    println!("...");
+    println!("Now we try the aggregate coin-flipping.");
+
+    let b: Ciphertext = coin_flipping::values_sender(10usize, &pubkey.N);
+    println!("B generated 10 values: {:?}", b);
+    
+    let b_q: Vec<bool> = b.iter().map(|x| quadratic_residues::is_n(&(x.clone()), &privkey.p, &privkey.q)).collect::<Vec<_>>();
+    println!("Their quadratic residuoity is as follows: {:?}", b_q);
+
+    let a: Plaintext = coin_flipping::values_receiver(&pubkey, &b);
+    println!("A did 10 guesses: {:?}", a);
+
+    let c: Ciphertext = crypto_system::encrypt(&a, &pubkey);
+    println!("A encrypted their guesses to send them to B: {:?}", c);
+    
+    let a_d: Plaintext = crypto_system::decrypt(&c, &privkey);
+    print!("B decrypted A's guesses: {:?}. They ", a_d);
+    if a_d != a {print!("do not ");}
+    println!("match what A sent.");
+
+    let m: Plaintext = coin_flipping::values_checker(&b, &a, &privkey);
+    println!("Here are the results of the coin flips: {:?}", m);
+
+    println!("...");
+    println!("Now let's check that aggregate coin-flipping is not biased.");
+    println!("Generating 10000 values...");
+    let b: Ciphertext = coin_flipping::values_sender(10000usize, &pubkey.N);    
+    println!("Generated 10000 values...");
+    let a: Plaintext = coin_flipping::values_receiver(&pubkey, &b);
+    println!("Generated 10000 guesses...");
+    let m: Plaintext = coin_flipping::values_checker(&b, &a, &privkey);
+    println!("Checked 10000 guesses...");
+    let n: usize = m.iter().map(|&x| if x {1usize} else {0usize}).sum();
+    let f = (n as f64) / (10000f64);
+    println!("Frequency of true over 10000 flips: {}", f); 
     
 }
